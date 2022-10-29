993b53eed80c934b8f5851bee5238aeb4994c333
Maged <19859654+magedragheb@users.noreply.github.com>
2022-10-29 14:34:24 +0200

    Add EncryptedPassword

---
+++ Data/Migrations/20221029123310_EncryptPassword.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBlazorVault.Data.Migrations
+{
+    public partial class EncryptPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "Secrets",
+                newName: "EncryptedPassword");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EncryptedPassword",
+                table: "Secrets",
+                newName: "HashedPassword");
+        }
+    }
+}
--- Data/Secret.cs
+++ Data/Secret.cs
@@ -5,7 +5,7 @@ public class Secret
     public Guid Id { get; set; }
     public string Title { get; set; } = "";
     public string UserName { get; set; } = "";
-    public string HashedPassword { get; set; } = "";
+    public string EncryptedPassword { get; set; } = "";
     public bool? Favorite { get; set; }
     public string? Notes { get; set; }
     public IdentityUser? User { get; set; }
